e64b50c2211d6da3472334eade8645b33d0f3f19
Brittany Blake <brittany.blake49@gmail.com>
2020-05-26 20:34:38 +0200

    Add new form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,25 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def new
+         @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = 'Article successfully saved'
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 end
