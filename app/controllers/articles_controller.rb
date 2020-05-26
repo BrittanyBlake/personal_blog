b4ea9f3a57597d3bdc3fbd4d0288916a2777fd49
Brittany Blake <brittany.blake49@gmail.com>
2020-05-26 21:19:27 +0200

    DRY up code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
 class ArticlesController < ApplicationController
     include ArticlesHelper
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
 
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -24,11 +25,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         @article.update(article_params)
          if @article.save
             flash[:notice] = "Article '#{@article.title}' successfully edited"
@@ -39,8 +38,12 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to @article
+    end
+
+    private
+    def set_article
+        @article = Article.find(params[:id])
     end
 end
