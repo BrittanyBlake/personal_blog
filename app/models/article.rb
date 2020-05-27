3943b1e08b8fd45ff9458fb4ad8d1e20258a042d
Brittany Blake <brittany.blake49@gmail.com>
2020-05-27 17:25:31 +0200

    style form partial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
-    validates :body, presence: true, length: {minimum: 10, maximum: 500}
+    validates :body, presence: true, length: {minimum: 10, maximum: 5000}
 end
