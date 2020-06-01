44c5f4450d8e27795198c63b5da9819c1f8fbec0
Brittany Blake <brittany.blake49@gmail.com>
2020-06-01 19:53:46 +0200

    Create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_141340) do
+ActiveRecord::Schema.define(version: 2020_06_01_175032) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
