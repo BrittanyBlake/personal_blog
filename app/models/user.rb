10306bdbff983dc600d6b0519ffe3f21e522c1f6
Brittany Blake <brittany.blake49@gmail.com>
2020-06-01 20:31:33 +0200

    Add a secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: {minimum:3, maximum:25}, uniqueness: {case_sensitive:false},
               format:{with:VALID_EMAIL_REGEX}
+    has_secure_password
 end
