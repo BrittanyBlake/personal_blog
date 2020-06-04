2471cbcf104c95bea90ff2d9721672632a845310
Brittany Blake <brittany.blake49@gmail.com>
2020-06-04 18:03:35 +0200

    Add users index page and nav link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,8 +32,13 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @articles = @user.articles
-   
     end
+
+    def index
+        @users = User.all
+    end
+
+    
 
     private
     def user_params
