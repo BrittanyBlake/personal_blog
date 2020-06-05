33539813940dab05598698e96b0988479ffef583
Brittany Blake <brittany.blake49@gmail.com>
2020-06-05 15:01:40 +0200

    make set user method private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
 
     def new
         @user = User.new
@@ -15,11 +16,9 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         @user.update(user_params)
         if @user.save
             flash[:notice] = "You're account has been successfully edited, #{@user.username}."
@@ -30,7 +29,6 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles
     end
 
@@ -42,6 +40,10 @@ class UsersController < ApplicationController
     
 
     private
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
