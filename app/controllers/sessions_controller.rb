c2dae78cc359751da511360de090ae60cf5cb519
Brittany Blake <brittany.blake49@gmail.com>
2020-06-05 15:21:26 +0200

    add create session method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email:params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            flash[:notice] = 'Login successful'
+            redirect_to user
+        else
+            flash.now[:alert] = 'Login unsuccessful, please try again'
+            render 'new'
+        end
     end
 
     def destroy
