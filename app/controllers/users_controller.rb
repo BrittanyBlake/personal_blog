7bccb72b9fa61a69a19c16df2dba09e87e405060
Brittany Blake <brittany.blake49@gmail.com>
2020-06-06 12:46:45 +0200

    add user partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
-        @users = User.paginate(page: params[:page], per_page: 10)
+        @users = User.paginate(page: params[:page], per_page: 6)
     end
 
     
