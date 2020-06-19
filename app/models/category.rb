d4973ee0a1246afa9de58490a3af0d96254aae57
Brittany Blake <brittany.blake49@gmail.com>
2020-06-19 17:25:29 +0200

    test if category name is too long or short

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, length: {maximum:25, minimum:3}
     validates_uniqueness_of :name
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -21,4 +21,14 @@ class CategoryTest < ActiveSupport::TestCase
         @category2 = Category.new(name:"sports")
         assert_not @category2.valid?
     end
+
+    test "name should not be too long" do
+        @category.name = "a" * 26
+        assert_not @category.valid?
+    end
+
+    test "name should not be too short" do
+        @category.name = "aa"
+        assert_not @category.valid?
+    end
 end
