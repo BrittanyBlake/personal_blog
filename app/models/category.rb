66d5923c4c4bf0706307e4735e4308f76dc4eb00
Brittany Blake <brittany.blake49@gmail.com>
2020-06-19 17:23:10 +0200

    test for uniqueness of category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     validates :name, presence: true
+    validates_uniqueness_of :name
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
+
+    def setup
+        @category = Category.new(name:"sports")
+    end
+
     test "category should be valid" do
         @category = Category.new(name:"sports")
         assert @category.valid?
     end
 
     test "name should be present" do
-        @category = Category.new(name:" ")
+        @category.name = " "
         assert_not @category.valid?
+    end
+
+    test "name should be unique" do
+        @category.save
+        @category2 = Category.new(name:"sports")
+        assert_not @category2.valid?
     end
 end
